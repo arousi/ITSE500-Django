6e3464b2908527527d7728599aeb4cca3b339dc3
vsc-sanad <41429866+arousi@users.noreply.github.com>
2025-09-22 02:44:01 +0200

    react build files

--- prompeteer_server/settings.py
+++ prompeteer_server/settings.py
@@ -454,7 +454,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    # BASE_DIR / 'frontend_build' / 'static',
+    BASE_DIR / 'frontend_build' / 'static',
 ]
 # Media (uploaded encrypted blobs)
 MEDIA_URL = '/media/'

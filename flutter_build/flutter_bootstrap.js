ca1acbd0b1662af87d0f60fc62bd74c659c5d645
vsc-sanad <41429866+arousi@users.noreply.github.com>
2025-09-24 02:09:32 +0200

    Refactor code structure for improved readability and maintainability

--- flutter_build/flutter_bootstrap.js
+++ flutter_build/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "20250924-1"
+    serviceWorkerVersion: "3149651331"
   }
 });
